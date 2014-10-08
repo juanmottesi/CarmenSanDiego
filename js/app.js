ec5c72e79ea2e6e5f808fb66d6995811039ef376
juanmottesi <juannpm159@gmail.com>
2014-10-08 19:15:58 -0300

    Se agrega un model

--- js/app.js
+++ js/app.js
@@ -56,10 +56,10 @@
     
     this.fin = function(){
       if(this.ordenDeArresto === this.detective.caso.villano){
-        alert("Ganaste!!!");
+        return ("Ganaste!!!");
       }
       else{
-        alert("Perdiste T_T");
+        return ("Perdiste T_T");
       }
     };
         
@@ -77,6 +77,60 @@
     };
   });
 
+  app.controller('ModalController', function($scope){
+    $scope.showModal = false;
+    $scope.toggleModal = function(){
+      $scope.showModal = !$scope.showModal;
+    };
+  });
+
+
+//directiva
+  
+  app.directive('modal', function () {
+    return {
+      template: '<div class="modal fade">' + 
+          '<div class="modal-dialog">' + 
+            '<div class="modal-content">' + 
+              '<div class="modal-header">' + 
+                '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' + 
+                '<h4 class="modal-title">{{ title }}</h4>' + 
+              '</div>' + 
+              '<div class="modal-body" ng-transclude></div>' + 
+            '</div>' + 
+          '</div>' + 
+        '</div>',
+      restrict: 'E',
+      transclude: true,
+      replace:true,
+      scope:true,
+      link: function postLink(scope, element, attrs) {
+        scope.title = attrs.title;
+
+        scope.$watch(attrs.visible, function(value){
+          if(value == true)
+            $(element).modal('show');
+          else
+            $(element).modal('hide');
+        });
+
+        $(element).on('shown.bs.modal', function(){
+          scope.$apply(function(){
+            scope.$parent[attrs.visible] = true;
+          });
+        });
+
+        $(element).on('hidden.bs.modal', function(){
+          scope.$apply(function(){
+            scope.$parent[attrs.visible] = false;
+          });
+        });
+      }
+    };
+  });
+  
+  
+//directiva  
 
 // Dummys "obj"?
 
